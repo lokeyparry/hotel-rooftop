bf52d22c67712ba3fa0496ef9a91b64252d273c6
Parvej Ansari <116214778+lokeyparry@users.noreply.github.com>
2024-08-26 19:45:35 +0530

    d

--- frontend/src/redux/features/blogs/blogsApi.js
+++ frontend/src/redux/features/blogs/blogsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const blogApi = createApi({
     reducerPath: 'blogsApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://parvejhotel-y9qa.vercel.app/api/',
+        baseUrl: 'https://parvejhotel-y9qa.vercel.app/api',
         credentials: 'include'
     }),
     tagTypes: ['Blogs'],
