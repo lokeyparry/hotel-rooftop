0483fb6d4eeb26e52f37e55f84879b586c8b6096
Parvej Ansari <116214778+lokeyparry@users.noreply.github.com>
2024-08-26 15:18:11 +0530

    j

--- frontend/src/redux/features/auth/authApi.js
+++ frontend/src/redux/features/auth/authApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const authApi = createApi({
     reducerPath: 'authApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://hotel-rooftop.vercel.app/api/auth",
+        baseUrl: "https://parvejansarihotels07.vercel.app/api/auth",
         credentials: "include",
     }),
     endpoints: (builder) => ({
